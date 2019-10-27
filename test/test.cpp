0dfd31d35d1925a6a2b17efdbb7dc493497f455c
Andrei Yaskovets <andreiyaskovets@Andreis-MacBook-Pro.local>
2019-10-27 20:51:23 +0300

    Test .cpp file formatting

--- test/test.cpp
+++ test/test.cpp
@@ -10,20 +10,20 @@ int main(int argc, char** argv)
     const size_t msg_bits = sizeof(msg) * 8;
 
     ret = kjb_insert("./images/test_2.jpg",
-                        "./images/test_write.bmp",
-                        msg,
-                        msg_bits,
-                        42,
-                        0.2);
+                     "./images/test_write.bmp",
+                     msg,
+                     msg_bits,
+                     42,
+                     0.2);
     if (ret != ERR_OK)
     { std::cout << "ERROR!" << std::endl; }
 
     unsigned char buffer[sizeof(msg) * 8];
     ret = kjb_extract("./images/test_write.bmp",
-                        buffer,
-                        msg_bits,
-                        42,
-                        2);
+                      buffer,
+                      msg_bits,
+                      42,
+                      2);
     if (ret != ERR_OK)
     {
         std::cout << "ERROR!" << std::endl;
