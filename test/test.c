751d0ae870c9d9e7a5d678bb39abe8dd24d10d81
Andrei Yaskovets <andreiyaskovets@Andreis-MacBook-Pro.local>
2019-11-30 14:11:06 +0300

    Array size fix in test

--- test/test.c
+++ test/test.c
@@ -11,7 +11,6 @@ int main(int argc, char** argv)
     const char *res_path = "./images/test_with_message.bmp";
 
     const unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '!' };
-    const unsigned int msg_bits = sizeof(msg) * 8;
 
     const unsigned int seed = rand();
 
@@ -19,14 +18,14 @@ int main(int argc, char** argv)
     const float rob = .5f;
     const float den = 1.f;
 
-    ret = kjb_insert(cnt_path, res_path, msg, msg_bits, seed, red, rob, den);
+    ret = kjb_insert(cnt_path, res_path, msg, sizeof(msg) * 8, seed, red, rob, den);
     if (ret != ERR_OK)
     {
         printf("Insert error: %d\n", ret);
         return ret;
     }
 
-    unsigned char buf[msg_bits];
+    unsigned char buf[sizeof(msg) * 8];
     const unsigned int buf_bits = sizeof(buf) * 8;
 
     const unsigned int nbh = 2;
@@ -39,28 +38,28 @@ int main(int argc, char** argv)
     }
 
     printf("Original message:\n[");
-    for (size_t i = 0; i < msg_bits / 8; ++i)
+    for (size_t i = 0; i < sizeof(msg) * 8 / 8; ++i)
     {
         printf("%c", (char)msg[i]);
     }
     printf("]\n");
 
     printf("Extracted message:\n[");
-    for (size_t i = 0; i < msg_bits / 8; ++i)
+    for (size_t i = 0; i < sizeof(msg) * 8 / 8; ++i)
     {
         printf("%c", (char)buf[i]);
     }
     printf("]\n");
 
     printf("Original message (binary):\n[");
-    for (size_t i = 0; i < msg_bits; ++i)
+    for (size_t i = 0; i < sizeof(msg) * 8; ++i)
     {
         printf("%d", ((msg[i / 8] & (1 << (i % 8))) != 0));
     }
     printf("]\n");
 
     printf("Extracted message (binary):\n[");
-    for (size_t i = 0; i < msg_bits; ++i)
+    for (size_t i = 0; i < sizeof(msg) * 8; ++i)
     {
         printf("%d", ((buf[i / 8] & (1 << (i % 8))) != 0));
     }
