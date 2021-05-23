961d8b958d9c4911f783f278114584e3cacc17ec
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2021-05-23 14:46:24 +0300

    Add license text

--- src/msg.cpp
+++ src/msg.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019-present, Andrei Yaskovets
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include "msg.hpp"
 
 Message::Message(const byte_t* _data, size_t _bits)
